3bde0c2ee38c7dad536520be5b13d19c4916ec25
Poff Poffenberger <bryant.poff@gmail.com>
2014-03-22 21:18:47 -0500

    Updating the path to the wav in the main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	//Load up audio
 	var audio = new Audio();
-	audio.setAttribute('src', 'damn.wav');
+	audio.setAttribute('src', 'media/damn.wav');
 
     $('.portrait').click(function() {
     	audio.currentTime = 0;
