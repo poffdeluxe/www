821104cf8afe19eef24878d9fe004d9eb2d6beda
Poff Poffenberger <bryant.poff@gmail.com>
2014-04-08 22:09:06 -0500

    Cleaned up terrible js identation

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
 	var audio = new Audio();
 	audio.setAttribute('src', 'media/damn.wav');
 
-    $('.portrait').click(function() {
-    	audio.currentTime = 0;
-    	audio.play();
+  $('.portrait').click(function() {
+		audio.currentTime = 0;
+  	audio.play();
 	});
 
 	//Add animated class
 	var fadeInArray = [$('.contact-group'), $('.description')];
 	fadeInArray.forEach(function($elem) {
 		$elem.addClass('animated fadeInUp')
-	})
+	});
 });
