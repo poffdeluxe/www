1ae8ba3ae0791bc1034d65aec6f4ff14101be074
Poff Poffenberger <poffdeluxe@gmail.com>
2016-08-10 16:52:50 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -14,43 +14,4 @@ $(document).ready(function() {
 	fadeInArray.forEach(function($elem) {
 		$elem.addClass('animated fadeInUp')
 	});
-
-	//Set-up last.fm
-	var lastfm = new LastFM({
-		apiKey    : 'cb1718d214061dee6228712d03f8e7e7',
-		apiSecret : '4561d99167199b8ebd932db5121c7c3e'
-	});
-	lastfm.user.getRecentTracks({
-		user: 'poffdeluxe',
-		limit: 1
-	}, {
-		success: function(data) {
-			var recentTracks = data.recenttracks.track;
-			if(recentTracks.length > 0) {
-				var track = recentTracks[0];
-
-				if(!track['@attr']) {
-					return;
-				}
-
-				if(!track['@attr']['nowplaying']) {
-					return;
-				}
-
-				// If we get here, we're scrobblin' rn
-				var artistName = track.artist['#text'];
-				var trackName = track.name;
-
-				var $music = $('#now-playing');
-				var $musicTitle = $music.find('#music-title');
-
-				$musicTitle.text(trackName + ' by ' + artistName);
-				$music.fadeIn();
-			}
-		},
-		error: function(code, message) {
-			console.log("Couldn't load Last.fm");
-		}
-	});
-
 });
